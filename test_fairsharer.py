85238706fad1d536d4336fdeb94cb086ef84c58f
Julian Mäder <115080032+Julimaeder@users.noreply.github.com>
2023-02-04 00:38:43 +0100

    Update test_fairsharer.py

--- test_fairsharer.py
+++ test_fairsharer.py
@@ -5,11 +5,11 @@ def test_fair_sharer():
     values = [0, 1000, 800, 0]
     num_iterations = 2
     expected_output = [100.0, 890.0, 720.0, 90.0]
-    output = fairsharer(values, num_iterations)
+    output = fair_sharer(values, num_iterations)
     assert output == expected_output
     #Test 2:
     values = [0, 1000, 800, 0]
     num_iterations = 2
     expected_output = [100.0, 890.0, 720.0, 90.0]
-    output = fairsharer(values, num_iterations)
+    output = fair_sharer(values, num_iterations)
     assert output == expected_output
